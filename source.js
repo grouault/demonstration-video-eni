48f22284a4d1f9eb3f72276fa8eef4cf54e5fdd4
Gildas Rouault <g.rouault@groupe-pomona.fr>
2020-09-19 16:04:20 +0200

    Ajout Maël

--- source.js
+++ source.js
@@ -5,9 +5,8 @@ function hello(){
   console.log("bonjour tout le monde");
   console.log("bonjour Gildas");
   console.log("Bonjour Aurélie");
-  console.log("Bonjour Alphonse");
-  console.log("Bonjout Toto");
+  console.log("Bonjour Maël");
   console.log("Bonjour Max");
-  console.log("Bonjour Einstein 4");
+
 }
 hello();
